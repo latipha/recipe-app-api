96fe916f8fd6f9ecf77d56616d8ba3aabeea309a
latipha <sherifa999@gmail.com>
2023-04-08 22:15:13 +0600

    configuring database

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+"""
+Calculator functions 
+"""
+def add (x,y):
+    """Add x and y and return a result."""
+    return x + y
+
+def substract(x,y):
+    """Substract y from x and return result"""
+    return y - x
