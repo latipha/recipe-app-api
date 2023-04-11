aeaeb1da371370b688a6452111e67cc95a093bf9
latipha <sherifa999@gmail.com>
2023-04-11 17:50:07 +0600

    added recipe api

---
+++ app/recipe/urls.py
@@ -0,0 +1,21 @@
+"""
+Url mappings for the recipe apps.
+"""
+
+from django.urls import (
+    path,
+    include
+)
+
+from rest_framework.routers import DefaultRouter
+
+from recipe import views
+
+router = DefaultRouter()
+router.register('recipes', views.RecipeViewSet)
+
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls))
+]
